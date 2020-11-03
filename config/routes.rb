d9c30a406aa4fa69e91787a236c21b2105809787
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2020-11-03 14:39:15 +0100

    Challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
